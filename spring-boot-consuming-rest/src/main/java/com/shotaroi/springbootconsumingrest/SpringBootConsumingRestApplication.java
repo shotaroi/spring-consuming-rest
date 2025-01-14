ec00fa88c219eafc16dbdae1d356f063eb1c81cf
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-14 11:37:00 +0100

    Create project

---
+++ spring-boot-consuming-rest/src/main/java/com/shotaroi/springbootconsumingrest/SpringBootConsumingRestApplication.java
@@ -0,0 +1,13 @@
+package com.shotaroi.springbootconsumingrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConsumingRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConsumingRestApplication.class, args);
+    }
+
+}
