35934866ba7de27afb1987e1173e606b53910883
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-14 12:03:48 +0100

    Delete Value.java

--- spring-boot-consuming-rest/src/main/java/com/shotaroi/springbootconsumingrest/Value.java
+++
@@ -1,6 +0,0 @@
-package com.shotaroi.springbootconsumingrest;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class Value(Long id, String quote) {
-}
